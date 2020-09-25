724dad2f64a8fe27b33ebf1e24eee73e0fdcc0ab
Sereja SMERECHEVSKIY <serejasmerechevskiy@o-s-i.org>
2020-09-25 18:51:59 +0300

    add base for redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
