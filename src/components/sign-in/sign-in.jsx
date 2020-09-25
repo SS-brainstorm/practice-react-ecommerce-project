384ae8bbe4d15fcddcafbe97139a22306d0b1e59
Sereja SMERECHEVSKIY <serejasmerechevskiy@o-s-i.org>
2020-09-25 12:56:11 +0300

    finish sign-in component

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import FormInput from "../form-input/form-input";
 import Button from "../button/button";
 
-import { signInWithGoogle } from "../../firebase/firebase";
+import { auth, signInWithGoogle } from "../../firebase/firebase";
 
 import "./sign-in.scss";
 
 const SignIn = () => {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     password: '',
     email: '',
-  });
+  }
+  const [formData, setFormData] = useState({ ...initialFormData });
 
   const handleInput = (event) => {
     const { name, value } = event.target;
@@ -21,8 +22,17 @@ const SignIn = () => {
     });
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
+
+    const { password, email } = formData;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setFormData({ ...initialFormData });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
@@ -32,8 +42,8 @@ const SignIn = () => {
         <p className="form-box__teaser">Sign in with your email and password</p>
       </div>
       <form action="" className="form form-box__form" onSubmit={handleSubmit}>
-        <FormInput className="form__field" onChange={handleInput} name="email" label="Email" />
-        <FormInput className="form__field" onChange={handleInput} name="password" label="Password" />
+        <FormInput type="email" className="form__field" onChange={handleInput} name="email" label="Email" />
+        <FormInput type="password" className="form__field" onChange={handleInput} name="password" label="Password" />
         <div className="form__actions">
           <Button type="submit">Sign in</Button>
           <Button type="button" mode="blue" onClick={signInWithGoogle}>Sign in with google</Button>
