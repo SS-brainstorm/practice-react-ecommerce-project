c116490417e3a7c7034bf282993de17cb016ed1f
Sereja Smerechevskiy <sereja.smerechevskiy.stratis@gmail.com>
2020-10-12 11:35:51 +0300

    - connect cart-button to redux

--- src/components/cart-button/cart-button.jsx
+++ src/components/cart-button/cart-button.jsx
@@ -6,22 +6,31 @@ import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg";
 import "./cart-button.scss";
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
 
-const CartButton = ({ className, toggleCartHidden, ...rest }) => {
+const CartButton = ({ className, toggleCartHidden, cartItems, ...rest }) => {
     const buttonClassName = classnames({
         'cart-button': true,
         [className]: className
     });
 
+    const cartItemsCount = cartItems.reduce((cur, next) => {
+        cur += next.quantity;
+        return cur;
+    }, 0);
+
     return (
         <button className={buttonClassName} onClick={toggleCartHidden} {...rest}>
             <ShoppingIcon className="cart-button__icon" />
-            <span className="cart-button__count">0</span>
+            <span className="cart-button__count">{cartItemsCount}</span>
         </button>
     );
 };
 
+const mapStateToProps = ({ cart }) => ({
+    cartItems: cart.cartItems,
+});
+
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()),
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartButton);
+export default connect(mapStateToProps, mapDispatchToProps)(CartButton);
